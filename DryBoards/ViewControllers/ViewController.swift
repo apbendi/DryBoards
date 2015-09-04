747a7f4f8351d067bb143436694801a86eb92c34
Ben DiFrancesco <ben.difrancesco@gmail.com>
2015-09-04 11:34:58 -0400

    Apply some styling to the root view

--- DryBoards/ViewControllers/ViewController.swift
+++ DryBoards/ViewControllers/ViewController.swift
@@ -2,9 +2,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableButton: UIButton!
+    @IBOutlet weak var panesButton: UIButton!
+    @IBOutlet weak var staticButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        ViewController.styleButtons([self.tableButton, self.panesButton, self.staticButton])
+    }
+
+    static func styleButtons(buttons: [UIButton]) {
+        for button in buttons {
+            button.layer.cornerRadius = 2.0
+        }
     }
 }
 
